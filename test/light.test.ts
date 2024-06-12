30c45ffe0118fb85598c2b1be6b5e272be273291
buzzing <buzzing2017@gmail.com>
2024-06-12 16:32:13 +0800

    完成submitBlockHeader功能性测试

--- test/light.test.ts
+++ test/light.test.ts
@@ -2,7 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
 import { ErrorCode, BlockInfo } from "./constants";
-import { calculateBlockHash } from "./utils"
+import { utils } from "./utils"
 
 describe("Light Client", () => {
     async function deployFixture() {
@@ -10,9 +10,9 @@ describe("Light Client", () => {
         const accounts = await hre.ethers.getSigners();
 
         // 部署 Utils Library，并获取地址
-        const UTILS = await hre.ethers.getContractFactory("Utils");
-        const utils = await UTILS.deploy();
-        const utils_addr = String(await utils.getAddress());
+        const UTILS_CONTRACT = await hre.ethers.getContractFactory("Utils");
+        const utils_contract = await UTILS_CONTRACT.deploy();
+        const utils_addr = String(await utils_contract.getAddress());
 
         // 部署 Validate Library，并获取地址
         const VALIDATE = await hre.ethers.getContractFactory("Validate", {
@@ -30,7 +30,9 @@ describe("Light Client", () => {
                 Validate: validate_addr,
             },
         });
-        const light = await LIGHT.deploy(BlockInfo.GENESIS_HEADER, BlockInfo.GENESIS_HEIGHT);
+        // @dev ts 中传入的 bytes like 类型的十六进制串必须以 0x 开头
+        const genesis_header = "0x" + utils.getRawHeaderFromHeight(BlockInfo.GENESIS_HEIGHT);
+        const light = await LIGHT.deploy(genesis_header, BlockInfo.GENESIS_HEIGHT);
 
         return { accounts, LIGHT, light };
     }
@@ -45,22 +47,78 @@ describe("Light Client", () => {
 
             it("Should revert if the genesis height is not greater than 0", async () => {
                 const { LIGHT } = await loadFixture(deployFixture);
+                const genesis_header = "0x" + utils.getRawHeaderFromHeight(BlockInfo.GENESIS_HEIGHT);
 
-                await expect(LIGHT.deploy(BlockInfo.GENESIS_HEADER, 0)).to.be.revertedWith(ErrorCode.INVALID_GENESIS);
+                await expect(LIGHT.deploy(genesis_header, 0)).to.be.revertedWith(ErrorCode.INVALID_GENESIS);
             });
 
             it("Should set the right genesis header", async () => {
                 const { light } = await loadFixture(deployFixture);
 
+                // 最新区块高度和哈希已经更新
                 const [header, height] = await light.getLatestBlock();
-                expect(header).to.equal(calculateBlockHash(BlockInfo.GENESIS_HEADER));
+                const genesis_header = utils.getRawHeaderFromHeight(BlockInfo.GENESIS_HEIGHT);
+                const genesis_header_hash = "0x" + utils.calculateBlockHash(genesis_header);
+
+                expect(header).to.equal(genesis_header_hash);
                 expect(height).to.equal(BlockInfo.GENESIS_HEIGHT);
             });
         });
 
         // TODO: 如何获取 constructor 中的抛出的事件？
         describe("Events", () => {
-            it("Should emit a storeHeader event", async () => {});
+            it("Should emit a storeHeader event", async () => { });
+        });
+    });
+
+    describe("Submit Block Header", () => {
+        describe("Validation", () => {
+            it("Should revert if the header's length is not 80 bytes", async () => {
+                const { light } = await loadFixture(deployFixture);
+
+                await expect(light.submitBlockHeader("0x1234")).to.be.revertedWith(ErrorCode.INVALID_HEADER_SIZE);
+            });
+
+            it("Should revert if the previous block does not exist", async () => {
+                const { light } = await loadFixture(deployFixture);
+                // 存储的创世纪块为 #120097，获取一个前序块不存在的块头
+                const new_header = "0x" + utils.getRawHeaderFromHeight(120099);
+
+                await expect(light.submitBlockHeader(new_header)).to.be.revertedWith(ErrorCode.PREV_BLOCK_NOT_FOUND);
+            });
+
+            it("Should revert if the block already exists", async () => {
+                const { light } = await loadFixture(deployFixture);
+                // 存储的创世纪块为 #120097，尝试再次提交
+                const existing_header = "0x" + utils.getRawHeaderFromHeight(120097);
+
+                await expect(light.submitBlockHeader(existing_header)).to.be.revertedWith(ErrorCode.BLOCK_EXISTS);
+            });
+
+            it("Should revert if the header's proof of work is insufficient", async () => {
+                const { light } = await loadFixture(deployFixture);
+                // 存储的创世纪块为 #120097，获取 #120098
+                const header = utils.getRawHeaderFromHeight(120098);
+                // 修改 nonce 为 0，#120098 这么修改后，将不再满足难度要求
+                const new_header = "0x" + utils.changeHeaderNonce(header);
+
+                await expect(light.submitBlockHeader(new_header)).to.be.revertedWith(ErrorCode.INSUFFICIENT_POW);
+            });
+
+            it("Should submit the block header successfully", async () => {
+                const { light } = await loadFixture(deployFixture);
+                // 存储的创世纪块为 #120097，获取 #120098
+                const header = "0x" + utils.getRawHeaderFromHeight(120098);
+                // 计算哈希时去掉开头的 0x
+                const header_hash = "0x" + utils.calculateBlockHash(header.slice(2));
+
+                await expect(light.submitBlockHeader(header)).not.to.be.reverted;
+
+                // 最新区块高度和哈希已经更新
+                const [latest_header, latest_height] = await light.getLatestBlock();
+                expect(latest_header).to.equal(header_hash);
+                expect(latest_height).to.equal(120098);
+            });
         });
     });
 });
--- test/utils.ts
+++ test/utils.ts
@@ -1,4 +1,5 @@
 import * as crypto from 'crypto';
+import * as fs from 'fs';
 
 /**
  * 计算 SHA-256 哈希值
@@ -11,11 +12,11 @@ function sha256(buffer: Buffer): Buffer {
 
 /**
  * 由 raw block header 计算 block hash
- * @param blockHeader 一个以 0x 开头的 80 字节的十六进制字符串
- * @returns 一个以 0x 开头的 64 字节的十六进制字符串（大端序）
+ * @param blockHeader 一个 80 字节的十六进制字符串
+ * @returns 一个 64 字节的十六进制字符串（大端序）
  */
 export function calculateBlockHash(blockHeader: string): string {
-    const blockHeaderBuffer = Buffer.from(blockHeader.slice(2), 'hex');
+    const blockHeaderBuffer = Buffer.from(blockHeader, 'hex');
 
     if (blockHeaderBuffer.length !== 80) {
         throw new Error('Invalid block header length. Must be 80 bytes.');
@@ -28,5 +29,57 @@ export function calculateBlockHash(blockHeader: string): string {
     // 将小端序转换为大端序
     const blockHash = Buffer.from(secondHash.reverse());
 
-    return "0x" + blockHash.toString('hex');
+    return blockHash.toString('hex');
 }
+
+/**
+ * 从 raw_headers.json 文件中获取指定高度的 raw block header
+ * raw_headers.json 文件由 utils/get_raw_headers.py 生成
+ * @param height 区块高度。本测试中，必须在 120097 到 120103 之间
+ * @returns 对应高度的 raw block header，一个 80 字节的十六进制字符串
+ */
+export function getRawHeaderFromHeight(height: number): string {
+    if (height < 120097 || height > 120103) {
+        throw new Error('Invalid block height. Must be between 120097 and 120103.');
+    }
+
+    const data = fs.readFileSync('raw_headers.json', 'utf8');
+    const rawHeaders = JSON.parse(data);
+
+    return rawHeaders[height];
+}
+
+/**
+ * 修改 block header 的 nonce 为 0
+ * @param blockHeader raw block header
+ * @returns 将 header 的 nonce 修改为 0 后的 header
+ */
+export function changeHeaderNonce(blockHeader: string): string {
+    // 检查 header 长度是否为 80 字节（160 个字符）
+    if (blockHeader.length !== 160) {
+        throw new Error('Invalid block header length. Must be 80 bytes.');
+    }
+
+    // 将 header 转换为二进制数组
+    let bytesArray = ((header: string): Uint8Array => {
+        let bytesArray = new Uint8Array(header.length / 2);
+        for (let i = 0; i < header.length; i += 2) {
+            bytesArray[i / 2] = parseInt(header.slice(i, i + 2), 16);
+        }
+        return bytesArray;
+    })(blockHeader);
+
+    // 修改数组的最后四个字节（nonce）为 0
+    for (let i = 76; i < 80; i++) {
+        bytesArray[i] = 0;
+    }
+
+    // 将修改后的二进制数组转换为十六进制字符串
+    let newHeader = ((bytesArray: Uint8Array): string => {
+        return Array.from(bytesArray, byte => byte.toString(16).padStart(2, '0')).join('');
+    })(bytesArray);
+
+    return newHeader;
+}
+
+export * as utils from "./utils"
